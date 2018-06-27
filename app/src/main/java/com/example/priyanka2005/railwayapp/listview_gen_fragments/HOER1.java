0a1f904f12adc22002016d2d83626b975b27f576
Ashwinshankar98 <ashwinshankar5@gmail.com>
2018-06-27 23:54:15 +0530

    Added Asynctask

--- app/src/main/java/com/example/priyanka2005/railwayapp/listview_gen_fragments/HOER1.java
+++ app/src/main/java/com/example/priyanka2005/railwayapp/listview_gen_fragments/HOER1.java
@@ -1,5 +1,6 @@
 package com.example.priyanka2005.railwayapp.listview_gen_fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,25 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 
 import com.example.priyanka2005.railwayapp.R;
 import com.example.priyanka2005.railwayapp.utils.Constants;
 import com.github.barteksc.pdfviewer.PDFView;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
-import java.io.File;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HOER1 extends Fragment {
 
-
+    private PDFView pdfView;
     public HOER1() {
     }
 
@@ -34,13 +30,38 @@ public class HOER1 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate( R.layout.gen_hoer_1,container,false);
 
-        //vikas u need to replicate the below 4 lines and should edit 3rd line accordingly and add webview also to others.
-        WebView webView = (WebView)view.findViewById( R.id.webview );
-        webView.getSettings().setJavaScriptEnabled( true );
-        String pdf = new Constants().url_gen_hoer1;
-        webView.loadUrl("http://drive.google.com/viewerng/viewer?embedded=false&url=" + pdf);
-        webView.goBack();
+        //vikas u need to replicate the below 3 lines and should edit 2nd line accordingly and add webview also to others.
+
+
+        pdfView = (PDFView)view.findViewById( R.id.pdfviewer );
+        new RetrieverClass().execute(new Constants().url_gen_hoer1 );
         return view;
+    }
+
+
+    class RetrieverClass extends AsyncTask<String,Void,InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream=null;
+            try {
+                URL url = new URL( strings[0] );
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+
+                if(httpURLConnection.getResponseCode() == 200){
+                    inputStream = new BufferedInputStream( httpURLConnection.getInputStream() );
+                }
+            }
+            catch (IOException e){
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream).load();
+        }
     }
 
 }
